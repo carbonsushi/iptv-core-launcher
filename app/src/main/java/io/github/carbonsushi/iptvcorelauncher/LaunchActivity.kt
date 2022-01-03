805c71d6fd31b3acb7c84301d33f682d16667763
carbonsushi <94692480+carbonsushi@users.noreply.github.com>
2022-01-04 03:51:06 +0900

    Implement resolve mDNS

--- app/src/main/java/io/github/carbonsushi/iptvcorelauncher/LaunchActivity.kt
+++ app/src/main/java/io/github/carbonsushi/iptvcorelauncher/LaunchActivity.kt
@@ -1,26 +1,30 @@
 package io.github.carbonsushi.iptvcorelauncher
 
-import android.app.Activity
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import androidx.preference.PreferenceManager
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import net.posick.mdns.Lookup
+import okhttp3.HttpUrl.Companion.toHttpUrl
+import org.xbill.DNS.ARecord
+import org.xbill.DNS.DClass
+import org.xbill.DNS.Type
 
-class LaunchActivity : Activity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
+class LaunchActivity : AppCompatActivity() {
+    private fun startIntent(playlistUri: Uri) {
         val sendIntent = Intent().apply {
             flags = Intent.FLAG_ACTIVITY_NEW_TASK
             setClassName(
                 "ru.iptvremote.android.iptv.core",
                 "ru.iptvremote.android.iptv.core.ChannelsActivity"
             )
-            data = Uri.parse(
-                PreferenceManager.getDefaultSharedPreferences(this@LaunchActivity)
-                    .getString("playlist_address", "")
-            )
+            data = playlistUri
         }
         runCatching {
             startActivity(sendIntent)
@@ -28,5 +32,35 @@ class LaunchActivity : Activity() {
             Toast.makeText(this, R.string.start_activity_error, Toast.LENGTH_SHORT).show()
         }
         finishAndRemoveTask()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+        val playlistUrl = sharedPreferences.getString("playlist_address", "")!!
+        val playlistUri = Uri.parse(playlistUrl)
+        val playlistHttpUrl = playlistUrl.toHttpUrl()
+
+        if (sharedPreferences.getBoolean("resolve_mdns", false)) {
+            lifecycleScope.launch {
+                val records = withContext(Dispatchers.IO) {
+                    Lookup(playlistHttpUrl.host, Type.A, DClass.IN).lookupRecords()
+                }
+                if (records.isEmpty()) {
+                    startIntent(playlistUri)
+                } else {
+                    startIntent(
+                        Uri.parse(
+                            playlistHttpUrl.newBuilder()
+                                .host((records[0] as ARecord).address.hostAddress!!).build()
+                                .toString()
+                        )
+                    )
+                }
+            }
+        } else {
+            startIntent(playlistUri)
+        }
     }
 }
